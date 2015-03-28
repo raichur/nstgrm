b115cd23e2c0032b6f0317fd9ec014678c5c6a51
Joshua Raichur <hello@joshuaraichur.com>
2015-03-28 21:21:11 +0530

    login or sign up

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    var signUpActive = true
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
     func displayAlert(title: String, error: String) {
         var alert = UIAlertController(title: title, message: error, preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
@@ -21,7 +25,22 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var signUpToggleButton: UIButton!
     
+    
+    @IBAction func toggleSignUp(sender: AnyObject) {
+        
+        if signUpActive == true {
+            
+            signUpActive = false
+            signUpButton.setTitle(<#title: String?#>, forState: <#UIControlState#>)
+            
+        } else {
+            
+        }
+        
+    }
     
     @IBAction func signUp(sender: AnyObject) {
         
@@ -43,7 +62,20 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             var user = PFUser()
             user.username = username.text
             user.password = password.text
+            
+            activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
             user.signUpInBackgroundWithBlock {(succeeded: Bool!, signUpError: NSError!) -> Void in
+                
+                self.activityIndicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                
                 if signUpError == nil {
                     
                 } else {
