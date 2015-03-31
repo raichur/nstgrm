eb6512b4068707191e7755e3fc875ce6a90247be
Joshua Raichur <hello@joshuaraichur.com>
2015-03-31 21:29:42 +0530

    activity indicator

--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -52,7 +52,6 @@ class postViewController: UIViewController, UINavigationControllerDelegate, UIIm
                 
                 self.activityIndicator.stopAnimating()
                 UIApplication.sharedApplication().endIgnoringInteractionEvents()
-                gi
                 if success == false {
                     self.activityIndicator.stopAnimating()
                     UIApplication.sharedApplication().endIgnoringInteractionEvents()
