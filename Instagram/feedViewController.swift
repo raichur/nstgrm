cc0981d4c8b1acf15c162b3bc9b03aeb9ee1f519
Joshua Raichur <hello@joshuaraichur.com>
2015-04-11 10:08:00 +0100

    feedviewcontroller

---
+++ Instagram/feedViewController.swift
@@ -0,0 +1,13 @@
+//
+//  feedViewController.swift
+//  Instagram
+//
+//  Created by Joshua Raichur on 4/11/15.
+//  Copyright (c) 2015 Joshua Raichur. All rights reserved.
+//
+
+import UIKit
+
+class feedViewController: UITableViewController {
+
+}
