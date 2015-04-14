7292c459fe1bd08cd5222c9dc8e2c5a31afb6aa5
Joshua Raichur <hello@joshuaraichur.com>
2015-04-14 21:20:05 +0530

    finished instagram app

--- Instagram/feedViewController.swift
+++ Instagram/feedViewController.swift
@@ -14,26 +14,45 @@ class feedViewController: UITableViewController {
     var titles = [String]()
     var usernames = [String]()
     var images = [UIImage]()
+    var imageFiles = [PFFile]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var getFollowedUsersQuery = PFQuery(className: "Followers")
+        getFollowedUsersQuery.whereKey("follower", equalTo: PFUser.currentUser().username)
+        getFollowedUsersQuery.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]!, error: NSError!) -> Void in
+            if error == nil {
+                var followedUser = ""
+                for object in objects {
+                    followedUser = object["following"] as String
+                    
+                    var query = PFQuery(className: "Post")
+                    query.whereKey("username", equalTo: followedUser)
+                    query.findObjectsInBackgroundWithBlock {
+                        (objects: [AnyObject]!, error: NSError!) -> Void in
+                        if error == nil {
+                            for object in objects {
+                                self.titles.append(object["caption"] as String)
+                                self.usernames.append(object["username"] as String)
+                                self.imageFiles.append(object["image"] as PFFile)
+                                self.tableView.reloadData()
+                                
+                            }
+                        } else {
+                            
+                        }
+                    }
+                }
+            }
+        }
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
-        var query = PFQuery(className: "Post")
-        query.findObjectsInBackgroundWithBlock {
-            (objects: [AnyObject]!, error: NSError!) -> Void in
-            if error == nil {
-                for object in objects {
-                    self.titles.append(object["title"] as String)
-                    self.usernames.append(object["username"] as String)
-                }
-            } else {
-                
-            }
-        }
     }
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -51,8 +70,18 @@ class feedViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var myCell: cell = self.tableView.dequeueReusableCellWithIdentifier("myCell") as cell
         
-        myCell.title.text = "My title"
-        myCell.username.text = "Rob"
+        myCell.title.text = titles[indexPath.row]
+        myCell.username.text = usernames[indexPath.row]
+        
+        imageFiles[indexPath.row].getDataInBackgroundWithBlock{
+            (imageData: NSData!, error: NSError!) -> Void in
+            
+            if error == nil {
+                let image = UIImage(data: imageData)
+                myCell.postedImage.image = image
+            }
+            
+        }
         
         return myCell
     }
